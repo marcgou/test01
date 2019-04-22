ebe2905b79b0765e4d8290ceb3515ee4888f8de8
MarcGoufone <marc.mundo@goufone.com>
2019-04-22 13:47:50 +0200

    Test Ping3

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #test
-#test GIT
+import ping3
+try:
+    while 1:
+        test = ping3.ping('8.8.8.8', timeout=0.1)
+        print(test)
+except Exception as e:
+    print("type error: " + str(e))
